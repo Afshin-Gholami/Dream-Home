30bf168c0e803269333f1930b4b57de7b34ba7c1
viano.ir <gholamiafshin070@gmail.com>
2025-07-13 17:32:07 +0330

    finish project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
-const $= document
-const menuButton=$.querySelector('.nav-right__menu')
-const menuMobile=$.querySelector(".nav__menu-mobile")
+const $ = document;
+const menuButton = $.querySelector(".nav-right__menu");
+const menuMobile = $.querySelector(".nav__menu-mobile");
 
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("nav-right__menu--active");
+  menuMobile.classList.toggle("nav__menu-mobile--active");
+});
 
-menuButton.addEventListener("click",()=>{
-    menuButton.classList.toggle("nav-right__menu--active")
-    menuMobile.classList.toggle("nav__menu-mobile--active")
-})
+window.addEventListener("scroll", () => {
+  menuMobile.classList.remove("nav__menu-mobile--active");
+  menuButton.classList.remove("nav-right__menu--active");
+});
